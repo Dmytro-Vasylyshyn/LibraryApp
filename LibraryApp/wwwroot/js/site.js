f6b5e7f1177bb9cf0a07cacf844281bf9615a802
Dmytro-Vasylyshyn <167178470+Dmytro-Vasylyshyn@users.noreply.github.com>
2024-10-01 10:29:03 +0000

    Add

--- LibraryApp/wwwroot/js/site.js
+++ LibraryApp/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.querySelectorAll('.delete-button').forEach(button => {
+    button.addEventListener('click', function (e) {
+        if (!confirm('Ви впевнені, що хочете видалити цей елемент?')) {
+            e.preventDefault();
+        }
+    });
+});
